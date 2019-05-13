8c2aba7a96025cada9d8390901df3efde8f21b77
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-05-13 21:50:31 +0300

    title

--- lib/core.sh
+++ lib/core.sh
@@ -4,10 +4,37 @@ true; PTS_TRUE=$?
 false; PTS_FALSE=$?
 PTS_ERROR=2
 
-### Debug settings
-PTS_DEBUG=${DEBUG:-0}  # 0: Disabled 1: Enabled
+# 0: Disabled 1: Enabled
+PTS_DEBUG=${DEBUG:-0}  
+PTS_ALLOW_ROOT=${ALLOW_ROOT:-0}
+PTS_TITLE=${TITLE:-1}
 
 export PTS_TRUE
 export PTS_FALSE
 export PTS_ERROR
 export PTS_DEBUG
+
+_pts_check_user () {
+    if [ "$(whoami)" = "root" ]; then
+        if [ "${PTS_ALLOW_ROOT}" -eq 0 ]; then
+                _log_fatal "DO NOT run this script under root!"
+            fi
+    fi
+}
+_pts_set_terminal_title () {
+    if [ "${PTS_TITLE}" -eq 1 ]
+    then
+        # shellcheck disable=SC2059
+        printf "\033]0;${1}\007"
+    fi
+}
+
+_pts_get_title_from_file () {
+    if [ -e "${1}" ]
+    then
+        title="$(cat "${1}")"
+    else
+        title="${2:-Terminal}"
+    fi
+    echo "${title}"
+}
