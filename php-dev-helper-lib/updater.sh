cbd72acda46d160b3b340064d3e62744573ea2e4
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-05-15 18:44:59 +0300

    updater

--- php-dev-helper-lib/updater.sh
+++ php-dev-helper-lib/updater.sh
@@ -1,27 +1,47 @@
 #!/usr/bin/env sh
-_REPOSITORY="alecrabbit/sh-php-dev-helper"
-# _REPOSITORY="alecrabbit/php-package-template" # FOR DEBUG 
+__TMP_DIR=".tmp"
+_OWNER="alecrabbit"
+_PACKAGE="sh-php-dev-helper"
+_REPOSITORY="${_OWNER}/${_PACKAGE}"
+_LATEST_VERSION=""
 
 updater_run () {
     _log_debug "Updater: checking install"
-    _latest_version="$(github_get_latest_release "${_REPOSITORY}")"
-    if [ "${_latest_version}" = "" ]; then
+    _LATEST_VERSION="$(github_get_latest_release "${_REPOSITORY}")"
+    if [ "${_LATEST_VERSION}" = "" ]; then
         _log_debug "Updater: release not found"
         _log_debug "Updater: searching for tag"
-        _latest_version="$(github_get_tags "${_REPOSITORY}")"
-        if [ "${_latest_version}" = "" ]; then
+        _LATEST_VERSION="$(github_get_tags "${_REPOSITORY}")"
+        if [ "${_LATEST_VERSION}" = "" ]; then
             _log_fatal "No releases or tags found for repository '${_REPOSITORY}'"
         fi
     fi
-    _log_debug "Github last version: ${_latest_version}"
-    if version_update_needed "${_latest_version}"; then
-        _log_info "New version found: ${_latest_version}"
+    _log_debug "Github last version: ${_LATEST_VERSION}"
+    if version_update_needed "${_LATEST_VERSION}"; then
+        _log_comment "Current version: ${_VERSION}"
+        _log_info "New version found: ${_LATEST_VERSION}"
+        if core_check_if_dir_exists "${SCRIPT_DIR}/.git"
+        then
+            _log_fatal "It seems you are trying to update lib source dir"
+        fi
+        _log_info "Updater: processing"
+        __updater_download
     else
         _log_comment "No update needed"
     fi    
-    if core_check_if_dir_exists "${SCRIPT_DIR}/.git"
+}
+
+__updater_download () {
+    __dir="${WORK_DIR}/${__TMP_DIR}"
+    _log_debug "Removing '${__dir}'\n$(rm -rfv "${__dir}" 2>&1)"
+    _log_debug "Recreating '${__dir}'\n$(mkdir -p "${__dir}" 2>&1)"
+    _log_debug "Downloading to '${__dir}/${_PACKAGE}-${_LATEST_VERSION}'"
+    if cd "${__dir}" && wget -qO- "https://github.com/${_REPOSITORY}/archive/${_LATEST_VERSION}.tar.gz" | tar -xzv
     then
-        _log_fatal "It seems you are trying to update lib source dir"
+        _log_debug "Package downloaded"
+        _log_debug "Copying new files\n$(cp -rv "${__dir}/${_PACKAGE}-${_LATEST_VERSION}"/. "${SCRIPT_DIR}"/. 2>&1)"
+    else
+        _log_fatal "Error occurred during download"
     fi
-    _log_info "Updater: processing"
+    unset __dir
 }
