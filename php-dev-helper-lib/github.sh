a314360bc7f3fb5939a8dd1e434e2a94a7817a37
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-05-15 22:13:55 +0300

    github module

--- php-dev-helper-lib/github.sh
+++ php-dev-helper-lib/github.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env sh
 github_get_latest_release() {
-  curl --silent "https://api.github.com/repos/${1}/releases/latest" |   # Get latest release from GitHub api
-    grep '"tag_name":' |                                                # Get tag line
-    sed -E 's/.*"([^"]+)".*/\1/'                                        # Pluck JSON value
+    __body="$(curl --silent "https://api.github.com/repos/${1}/releases/latest")"   # Get latest release from GitHub api
+    echo "${__body}" | grep '"tag_name":' |                                         # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                                    # Pluck JSON value
 }
+
 github_get_tags() {
-  curl --silent "https://api.github.com/repos/${1}/tags" |              # Get tags from GitHub api
-    grep -m1 '"name":' |                                                # Get tag line
-    sed -E 's/.*"([^"]+)".*/\1/'                                        # Pluck JSON value
+    __body="$(curl --silent "https://api.github.com/repos/${1}/tags")"              # Get tags from GitHub api
+    echo "${__body}" | grep -m1 '"name":' |                                         # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                                    # Pluck JSON value
 }
+
 github_get_latest_version () {
     __latest="$(github_get_latest_release "${1}")"
     if [ "${__latest}" = "" ]; then
