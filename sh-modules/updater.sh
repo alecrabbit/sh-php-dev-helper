d2dde09d2ee41b3078755403350da068e1b88103
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-05-20 00:11:36 +0300

    updater

--- sh-modules/updater.sh
+++ sh-modules/updater.sh
@@ -13,15 +13,31 @@ updater_download () {
     console_debug "Removing '${__to_dir}'\n$(rm -rfv "${__to_dir}" 2>&1)"
     console_debug "Recreating '${__to_dir}'\n$(mkdir -pv "${__to_dir}" 2>&1)"
     console_debug "Downloading to '${__to_dir}/${__package}-${__version}'"
-    __result="$(cd "${__to_dir}" && wget -qOlog "https://github.com/${__repository}/archive/${__version}.tar.gz" | tar -xzv 2>&1)"
-    # shellcheck disable=SC2181
-    if [ $? -eq 0 ]; then
-        console_info "Package '${__package}-${__version}' downloaded"
-        return "${CR_TRUE}"
+
+    __result="$(wget -nv -O download.tar.gz -o /dev/stderr "https://github.com/${__repository}/archive/${__version}.tar.gz" 2>&1 )"
+    if [ $? -ne "${CR_TRUE}" ]
+    then
+        console_error "${__result}"
+    else 
+        __result="$(tar -xzvf download.tar.gz 2>&1)"
+        if [ $? -ne "${CR_TRUE}" ]; then
+            console_error "${__result}"
+        else
+            console_info "Package '${__package}-${__version}' downloaded"
+            return "${CR_TRUE}"
+        fi
     fi
+
+
+    # __result="$(cd "${__to_dir}" && wget -qOlog "https://github.com/${__repository}/archive/${__version}.tar.gz" | tar -xzv 2>&1)"
+    # # shellcheck disable=SC2181
+    # if [ $? -eq 0 ]; then
+    #     console_info "Package '${__package}-${__version}' downloaded"
+    #     return "${CR_TRUE}"
+    # fi
     
     console_debug "${__result}"
-    console_error "Possible cause: incorrect version $(colored_bold_cyan "'${__version}'")"
+    # console_error "Possible cause: incorrect version $(colored_bold_cyan "'${__version}'")"
 
     unset __to_dir __version __result __package __repository
     return "${CR_FALSE}"
