fc5c3ae354293d948f35b3185571ccd438fd4b3c
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-06-01 12:32:22 +0300

    wp 42

--- php-dev-helper-lib/sh-modules/console.sh
+++ php-dev-helper-lib/sh-modules/console.sh
@@ -26,6 +26,7 @@ console_error () {
     console_print "${EMOJI_ERROR:-}${_SERVICE}$(colored_bold_red "ERROR") $*\n" "${CR_TRUE}" "${CR_TRUE}"
 }
 console_fatal () {
+    core_set_terminal_title "${__TITLE}"
     console_print "${EMOJI_FATAL:-}${_SERVICE}$(colored_bold_red "FATAL") $*\n" "${CR_TRUE}" "${CR_TRUE}"
     exit "${CR_ERROR}"
 }
