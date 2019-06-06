f63f1a996dcfb873887929fa9cf4a383da7d32db
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-06-06 15:12:08 +0300

    wp 46

--- php-dev-helper-lib/include_pts.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-### Read options
-_pts_read_options "$@"
---
+++ php-dev-helper-lib/includers/include_pts.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+# shellcheck disable=SC1090
+. "${APPS_MODULES_DIR}/pts.sh"               # Image builder module
+
+### Read options
+_pts_read_options "$@"
