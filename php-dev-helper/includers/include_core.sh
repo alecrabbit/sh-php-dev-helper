508d95b2d5ba030365ccf7722ad4608d76de5ee5
Alec <alecrabbit@users.noreply.github.com>
2019-06-12 00:49:49 +0300

    Update include_core.sh

--- php-dev-helper/includers/include_core.sh
+++ php-dev-helper/includers/include_core.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env sh
+
+# Disable source following.
+#   shellcheck disable=SC1090,SC1091
+
 SCRIPT_NAME="$(basename "$0")"
 export SCRIPT_NAME
 
-
-# shellcheck disable=SC1090
 . "${CORE_MODULES_DIR}/core.sh"
-# shellcheck disable=SC1090
 . "${CORE_MODULES_DIR}/github.sh"
-# shellcheck disable=SC1090
 . "${CORE_MODULES_DIR}/docker.sh"
-# shellcheck disable=SC1090
 . "${CORE_MODULES_DIR}/git.sh"
-# shellcheck disable=SC1090
 . "${CORE_MODULES_DIR}/version.sh"
-# shellcheck disable=SC1090
 . "${CORE_MODULES_DIR}/updater.sh"
-# shellcheck disable=SC1090
 . "${CORE_MODULES_DIR}/notifier.sh"
