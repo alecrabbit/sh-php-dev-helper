7159cc1a7230ad10056faf6e82eba8cba5ffbbd0
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-06-14 14:49:17 +0300

    opt

--- php-dev-helper/includers/include_bdi.sh
+++ php-dev-helper/includers/include_bdi.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
-# shellcheck disable=SC1090
+# Disable source following.
+#   shellcheck disable=SC1090,SC1091
+
 . "${APPS_MODULES_DIR}/bdi.sh"               # Image builder module
 
 bdi_load_settings
--- php-dev-helper/includers/include_mmb.sh
+++ php-dev-helper/includers/include_mmb.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
-# shellcheck disable=SC1090
+# Disable source following.
+#   shellcheck disable=SC1090,SC1091
+
 . "${APPS_MODULES_DIR}/mmb.sh"               # Moomba module
 
