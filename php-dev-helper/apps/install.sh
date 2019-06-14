806f0ce5836740971a36e4409e0086531a79bb8e
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-06-14 18:02:17 +0300

    wp 57

---
+++ php-dev-helper/apps/install.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env sh
+
+install_load_settings () {
+    console_debug "Dummy: loading settings"
+}
+
+install_set_default_options () {
+    :
+}
+
+install_process_options () {
+    :
+}
+
+install_usage () {
+    echo "    $(colored_yellow "-d")                    - <dummy>"
+}
+
+install_export_options () {
+    :
+}
+
+install_cleanup () {
+    console_print ""
+    console_comment "Cleaning Up"
+    console_debug "Deleting files"
+    console_debug "$(core_get_realpath "apps/install.sh")"
+    console_debug "$(core_get_realpath "includers/include_install.sh")"
+    console_debug "$(core_get_realpath "install")"
+}
+install_read_options  () {
+    common_set_default_options
+    install_set_default_options
+    console_debug "install: Reading options"
+    while [ "${1:-}" != "" ]; do
+        __OPTION=$(echo "$1" | awk -F= '{print $1}')
+        __VALUE=$(echo "$1" | awk -F= '{print $2}')
+        case ${__OPTION} in
+            *)
+                common_read_option "install_usage" "${__OPTION}$([ "${__VALUE}" != "" ] && echo "=${__VALUE}")"
+                ;;
+        esac
+        shift
+    done
+    common_process_options
+    common_export_options
+    install_process_options
+    install_export_options
+    unset __OPTION __VALUE
+}
+
+install_show_settings () {
+    console_debug "Dummy: showing settings"
+}
+
+install_check_working_env  () {
+    console_debug "Dummy: check working env"
+}
---
+++ php-dev-helper/includers/include_install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+# Disable source following.
+#   shellcheck disable=SC1090,SC1091
+
+. "${APPS_MODULES_DIR}/install.sh"               # Install module
