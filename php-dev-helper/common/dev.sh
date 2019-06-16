c3dd40005085515033b4d7781279442faa385b89
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-06-16 14:03:53 +0300

    wp 64

--- php-dev-helper/common/dev.sh
+++ php-dev-helper/common/dev.sh
@@ -3,19 +3,6 @@
 console_debug ""
 console_debug "*** DEV MODULE BEGIN ***"
 
-SCRIPT_DIR="$(core_get_realpath "${SCRIPT_DIR}")"
-LIB_DIR="$(core_get_realpath "${LIB_DIR}")"
-WORK_DIR="$(core_get_realpath "${WORK_DIR}")"
-VERSION_FILE="$(core_get_realpath "${VERSION_FILE}")"
-BUILD_FILE="$(core_get_realpath "${BUILD_FILE}")"
-
-console_debug "Script name: ${SCRIPT_NAME}"
-console_debug "Script dir: ${SCRIPT_DIR}"
-console_debug "Lib dir: ${LIB_DIR}"
-console_debug "Work dir: ${WORK_DIR}"
-console_debug "VERSION file: ${VERSION_FILE}"
-console_debug "BUILD file: ${BUILD_FILE}"
-
 __hash="$(version_save_build_hash "${SCRIPT_DIR}" "${LIB_DIR}")"
 if [ $? -eq "${CR_TRUE}" ]
 then
