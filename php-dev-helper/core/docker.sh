e3f3c29f36cd50ad0f9e4077e3a7fef0533aa2c6
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-08-08 15:58:09 +0300

    Fixed: Uppercase dirs

--- php-dev-helper/core/docker.sh
+++ php-dev-helper/core/docker.sh
@@ -8,7 +8,7 @@
 #   $1 string working dir
 #   $2 string (optional)container name
 docker_compose_is_container_started () {
-    test "$(cd "${1}" && docker-compose ps 2>&1 | grep "${2:-$(basename "${1}")}.*Up")" != ""
+    test "$(cd "${1}" && docker-compose ps 2>&1 | grep "${2:-$(core_lowercase "$(basename "${1}")")}.*Up")" != ""
 }
 
 # Args:
@@ -23,5 +23,5 @@ docker_compose_is_debug_image_used () {
     # echo "'${__result}'" >&2
     # test "${__result}" != ""
     # test "$(cd "${1}" && docker-compose images 2>&1 | grep -A1 "${2:-$(basename "${1}")}.*${3:-debug}")" != ""
-    test "$(cd "${1}" && docker-compose images 2>&1 | grep -A1 "${2:-$(basename "${1}")}" | grep "${3:-debug}")" != ""
+    test "$(cd "${1}" && docker-compose images 2>&1 | grep -A1 "${2:-$(core_lowercase "$(basename "${1}")")}" | grep "${3:-debug}")" != ""
 }
